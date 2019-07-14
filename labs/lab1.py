075e12db2a6b8d1b9a9ad70b0a6319f21abe6cc6
spencerneveux <spencerneveux@gmail.com>
2019-07-14 11:03:16 -0700

    Test main method working

--- labs/lab1.py
+++ labs/lab1.py
@@ -6,7 +6,7 @@ import math
 # Set up
 #---------------
 def menu():
-	user_input = input('1) Time Freddy\n2) Time Susie\n3) Time Johnny\n4) Time Sally\n5) Quit\n')
+	user_input = input("1) Quit\n2) Time Freddy's\n3) Time Susie's\n4) Time Johnny's\n5) Time Sally's\n")
 	return user_input
 
 def collect_input():
@@ -27,7 +27,6 @@ def create_array(s, n):
 #---------------
 
 def freddy(array):
-	time_1 = time.clock()
 	max_num = 0
 	for i in range(0, len(array)):
 		for j in range(i, len(array)):
@@ -36,12 +35,9 @@ def freddy(array):
 				this_sum += array[k]
 			if this_sum > max_num:
 				max_num = this_sum
-	time_2 = time.clock()
-	print(f'Max Sum = {max_num}, Time taken = {time_2 - time_1}')
 	return max_num
 			
-def sophie(array):
-	time_1 = time.clock()
+def susie(array):
 	max_num = 0
 	for i in range(0, len(array)):
 		this_sum = 0
@@ -49,11 +45,11 @@ def sophie(array):
 			this_sum += array[j]
 			if this_sum > max_num:
 				max_num = this_sum
-	time_2 = time.clock()
-	print(f'Max Sum = {max_num}, Time taken = {time_2 - time_1}')
 	return max_num
 
 def johnny(array, left, right):
+	if right < 0: 
+		return 0
 	if left == right:
 		if array[left] > 0:
 			return array[left]
@@ -63,7 +59,6 @@ def johnny(array, left, right):
 	center = math.floor((left + right) / 2)
 	max_left_sum = johnny(array, left, center)
 	max_right_sum = johnny(array, center+1, right)
-	print(f'Left sum {max_left_sum} Right sum {max_right_sum}')
 
 	# Find max starting at center moving left
 	max_left_border = 0
@@ -76,18 +71,15 @@ def johnny(array, left, right):
 	# Find max starting at center moving right
 	max_right_border = 0
 	right_border = 0
-	for i in range(center, right+1):
+	for i in range(center+1, right+1):
 		right_border += array[i]
 		if right_border > max_right_border:
 			max_right_border = right_border
-
-	print(f'Max left: {max_left_sum} Max right: {max_right_sum}')
 
 	# the max sum is the largest of the three
 	return max(max_left_sum, max_right_sum, max_left_border+max_right_border)
 
 def sally(array):
-	time_1 = time.clock()
 	max_num = 0
 	this_sum = 0
 	for i in range(0, len(array)):
@@ -96,12 +88,79 @@ def sally(array):
 			max_num = this_sum
 		elif this_sum < 0:
 			this_sum = 0
-	time_2 = time.clock()
-	print(f'Max Sum = {max_num}, Time taken = {time_2 - time_1}')
 	return max_num
 
+#---------------
+# Test-Main
+#---------------
+def test_main():
+	array_1 = [11, -76, 34, 44, -98, 1, 21, -10, 9, 100]
+	array_2 = [21, 84, -74, 18, 92, -32, 66, 38, 91, -21, 1]
+	array_3 = []
+	array_4 = [-10, -94, -100, -23, -43, -75, -23, -86, -11, -32]
+	array_5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	array_6 = [21, 85, 38, 45, 24, 68, 22, 45, 92, 12]
 
+	# Testing Freddy
+	max_sum = freddy(array_1)
+	print(f'Freddy Max Sum = {max_sum}')
+	max_sum = freddy(array_2)
+	print(f'Freddy Max Sum = {max_sum}')
+	max_sum = freddy(array_3)
+	print(f'Freddy Max Sum = {max_sum}')
+	max_sum = freddy(array_4)
+	print(f'Freddy Max Sum = {max_sum}')
+	max_sum = freddy(array_5)
+	print(f'Freddy Max Sum = {max_sum}')
+	max_sum = freddy(array_6)
+	print(f'Freddy Max Sum = {max_sum}')
+	print()
 
+	# Testing Susie
+	max_sum = susie(array_1)
+	print(f'Susie Max Sum = {max_sum}')
+	max_sum = susie(array_2)
+	print(f'Susie Max Sum = {max_sum}')
+	max_sum = susie(array_3)
+	print(f'Susie Max Sum = {max_sum}')
+	max_sum = susie(array_4)
+	print(f'Susie Max Sum = {max_sum}')
+	max_sum = susie(array_5)
+	print(f'Susie Max Sum = {max_sum}')
+	max_sum = susie(array_6)
+	print(f'Susie Max Sum = {max_sum}')
+	print()
+
+	# Testing johnny
+	max_sum = johnny(array_1, 0, len(array_1)-1)
+	print(f'Johnny Max Sum = {max_sum}')
+	max_sum = johnny(array_2, 0, len(array_2)-1)
+	print(f'Johnny Max Sum = {max_sum}')
+	max_sum = johnny(array_3, 0, len(array_3)-1)
+	print(f'Johnny Max Sum = {max_sum}')
+	max_sum = johnny(array_4, 0, len(array_4)-1)
+	print(f'Johnny Max Sum = {max_sum}')
+	max_sum = johnny(array_5, 0, len(array_5)-1)
+	print(f'Johnny Max Sum = {max_sum}')
+	max_sum = johnny(array_6, 0, len(array_6)-1)
+	print(f'Johnny Max Sum = {max_sum}')
+	print()
+
+	# Testing sally
+	max_sum = sally(array_1)
+	print(f'Sally Max Sum = {max_sum}')
+	max_sum = sally(array_2)
+	print(f'Sally Max Sum = {max_sum}')
+	max_sum = sally(array_3)
+	print(f'Sally Max Sum = {max_sum}')
+	max_sum = sally(array_4)
+	print(f'Sally Max Sum = {max_sum}')
+	max_sum = sally(array_5)
+	print(f'Sally Max Sum = {max_sum}')
+	max_sum = sally(array_6)
+	print(f'Sally Max Sum = {max_sum}')
+
+	
 #---------------
 # Main
 #---------------
@@ -119,18 +178,29 @@ def main():
 	print(array)
 
 	# Run Freddy algorithm
-	freddy(array)
+	time_1 = time.perf_counter_ns()
+	max_num	= freddy(array)
+	elapsed_time = time.perf_counter_ns() - time_1
+	print(f'Max sum {max_num}, Elapsed Time {elapsed_time}')
 
-	# Run Sophie algorithm
-	sophie(array)
+	# Run Susie algorithm
+	time_1 = time.perf_counter_ns()
+	max_num = susie(array)
+	elapsed_time = time.perf_counter_ns() - time_1
+	print(f'Max sum {max_num}, Elapsed Time {elapsed_time}')
 
 	# Run Johnny algorithm
-	max_num = johnny(array, 0, len(array))
-	print(f'Johnny max {max_num}')
+	time_1 = time.perf_counter_ns()
+	max_num = johnny(array, 0, len(array)-1)
+	elapsed_time = time.perf_counter_ns() - time_1
+	print(f'Max sum {max_num}, Elapsed Time {elapsed_time}')
 
 	# Run Sally algorithm
-	sally(array)
+	time_1 = time.perf_counter_ns()
+	max_num = sally(array)
+	elapsed_time = time.perf_counter_ns() - time_1
+	print(f'Max sum {max_num}, Elapsed Time {elapsed_time}')
 
 
 if __name__ == '__main__':
-	main()
+	test_main()
